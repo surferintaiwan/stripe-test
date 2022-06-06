8445ca98a42daf311c98cd0b44a8320e406178c9
ShawnPan <shawnpan@ShawnPandeMacBook-Air.local>
2022-06-06 18:03:05 +0800

    add /register route

--- router/index.js
+++ router/index.js
@@ -7,4 +7,14 @@ router.get('/', async (req, res) => {
     res.render('pricing.html', { products });
 });
 
+router.post('/register', (req, res) => {
+    const plan = JSON.parse(req.body.plan);
+    plan.formatted = req.body.plan;
+  
+    res.render('register.html', {
+      productName: req.body.productName,
+      plan,
+    });
+  });
+
 module.exports = router;
